15d9ccb750f240be9b6b0193e8492ec5eadedf94
Faz Kay <fkydln@gmail.com>
2024-03-16 12:05:26 -0700

    MVP done.

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# ASSIGNMENT: Use unittest.TestCase methods to confirm that the addition and subtraction of date and timedelta
+# objects produce correct results
+from datetime import date, timedelta
+import unittest
 
 
-# Press the green button in the gutter to run the script.
+def add_days(start_date, num_days):
+    return start_date + timedelta(days=num_days)
+
+
+def subtract_days(start_date, num_days):
+    return start_date - timedelta(days=num_days)
+
+
+class TestDateOperations(unittest.TestCase):
+
+    def test_addition(self):
+        d1 = date(2000, 1, 1)
+        td = timedelta(weeks=2)
+        result = add_days(d1, td.days)
+        self.assertEqual(result, date(2000, 1, 15))
+
+    def test_subtraction(self):
+        d1 = date(2000, 1, 1)
+        td = timedelta(days=10)
+        result = subtract_days(d1, td.days)
+        self.assertEqual(result, date(1999, 12, 22))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    unittest.main()
