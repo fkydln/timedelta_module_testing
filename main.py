42d10be249bc6e6c8d6ceb8184846f0c41fb9b30
Faz Kay <fkydln@gmail.com>
2024-03-16 23:15:36 -0700

    Results printed for convenience.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# ASSIGNMENT: Use unittest.TestCase methods to confirm that the addition and subtraction of date and timedelta
-# objects produce correct results
 from datetime import date, timedelta
 import unittest
 
@@ -12,19 +10,21 @@ def subtract_days(start_date, num_days):
     return start_date - timedelta(days=num_days)
 
 
-class TestDateOperations(unittest.TestCase):
+class TestModule(unittest.TestCase):
 
     def test_addition(self):
         d1 = date(2000, 1, 1)
         td = timedelta(weeks=2)
         result = add_days(d1, td.days)
         self.assertEqual(result, date(2000, 1, 15))
+        print("test_addition test module is passed.")
 
     def test_subtraction(self):
         d1 = date(2000, 1, 1)
         td = timedelta(days=10)
         result = subtract_days(d1, td.days)
         self.assertEqual(result, date(1999, 12, 22))
+        print("test_subtraction test module is passed.")
 
 
 if __name__ == '__main__':
